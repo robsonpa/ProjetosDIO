1acfd24de393fb1d9650ba61405d522ba08d6210
Robson Parreiras <robson.rap@gmail.com>
2021-03-02 19:04:31 -0300

    Add  novos desafios

---
+++ Coordenadas_de_um_ponto/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Coordenadas_de_um_ponto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entrada = Console.ReadLine().Split(" ");
+            double x = double.Parse(entrada[0]);
+            double y = double.Parse(entrada[1]);
+
+
+            if (x == 0.0 && y == 0.0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0.0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0.0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.0 && y > 0.0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.0 && y < 0.0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+
+
+        }
+    }
+}
